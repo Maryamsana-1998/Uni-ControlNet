fe754053d7e27cfb96669516adb5cc78b08c554d
Maryamsana-1998 <msana.bee17seecs@seecs.edu.pk>
2024-07-04 19:15:37 +0900

    restructing testing code

--- training_global.sh
+++ training_global.sh
@@ -9,4 +9,4 @@
 #Your command to run the Python script
 #pip install torch torchvision compressai
 #source activate compressails
-python src/train/train.py --config-path ./configs/global_v15.yaml ---resume-path ckpt/init_global.ckpt --ckpt /checkpoints/global_all ---training-steps 200000 ---gpus 6 ---logdir global_logs/
+python src/train/train.py --config-path ./configs/global_v15.yaml ---resume-path ./ckpt/init_global.ckpt --ckpt ./checkpoints/global_all ---training-steps 200000 ---gpus 6 ---logdir ./global_logs/
