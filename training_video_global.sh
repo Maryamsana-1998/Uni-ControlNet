2ce391842f43284bb3293b79b43db089662d16e7
Maryamsana-1998 <msana.bee17seecs@seecs.edu.pk>
2024-07-11 15:19:44 +0900

    video data training updated

--- training_video_global.sh
+++ training_video_global.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --time=4-0  # Set the time limit to 3 hours
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:8
 #SBATCH --cpus-per-gpu=8
 #SBATCH --mem-per-gpu=29G
 #SBATCH -p batch_grad
@@ -9,4 +9,4 @@
 #Your command to run the Python script
 #pip install torch torchvision compressai
 #source activate compressails
-python src/train/train_video.py --config-path configs/video_uni_v15/global_v15.yaml ---training-steps 50000 --ckpt checkpoints/video_global/ ---gpus 4
+python src/train/train_video.py --config-path configs/video_uni_v15/global_v15.yaml ---resume-path ckpt/video_init_global.ckpt ---training-stelps 200000 --ckpt checkpoints/video_global/ ---batch-size 1 ---gpus 8
