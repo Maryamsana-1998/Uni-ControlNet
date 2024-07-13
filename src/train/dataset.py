392db5a2d54ea0b81ce2990569a729edb90b3e9c
Maryamsana-1998 <msana.bee17seecs@seecs.edu.pk>
2024-07-13 18:26:04 +0900

    laicon modifed train and dataset scripts

--- src/train/dataset.py
+++ src/train/dataset.py
@@ -1,35 +1,39 @@
 import os
 import random
-import cv2
+import cv2  # type: ignore
 import numpy as np
 
 from torch.utils.data import Dataset
 
-from .util import *
+from src.train.util import read_anno, keep_and_drop
 
 
 class UniDataset(Dataset):
-    def __init__(self,
-                 anno_path,
-                 image_dir,
-                 condition_root,
-                 local_type_list,
-                 global_type_list,
-                 resolution,
-                 drop_txt_prob,
-                 keep_all_cond_prob,
-                 drop_all_cond_prob,
-                 drop_each_cond_prob):
-        
+    def __init__(
+        self,
+        anno_path,
+        image_dir,
+        condition_root,
+        local_type_list,
+        global_type_list,
+        resolution,
+        drop_txt_prob,
+        keep_all_cond_prob,
+        drop_all_cond_prob,
+        drop_each_cond_prob,
+    ):
+
         file_ids, self.annos = read_anno(anno_path)
-        self.image_paths = [os.path.join(image_dir, file_id + '.png') for file_id in file_ids]
-        self.local_paths = {}
-        for local_type in local_type_list:
-            self.local_paths[local_type] = [os.path.join(condition_root, local_type, file_id + '.png') for file_id in file_ids]
+        self.image_paths = [
+            os.path.join(image_dir, file_id + ".jpg") for file_id in file_ids
+        ]
         self.global_paths = {}
         for global_type in global_type_list:
-            self.global_paths[global_type] = [os.path.join(condition_root, global_type, file_id + '.npy') for file_id in file_ids]
-        
+            self.global_paths[global_type] = [
+                os.path.join(condition_root, global_type, file_id + ".npy")
+                for file_id in file_ids
+            ]
+
         self.local_type_list = local_type_list
         self.global_type_list = global_type_list
         self.resolution = resolution
@@ -37,50 +41,51 @@ class UniDataset(Dataset):
         self.keep_all_cond_prob = keep_all_cond_prob
         self.drop_all_cond_prob = drop_all_cond_prob
         self.drop_each_cond_prob = drop_each_cond_prob
-    
+
     def __getitem__(self, index):
         image = cv2.imread(self.image_paths[index])
-        try:
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        except:
-            print(self.image_paths[index])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image = cv2.resize(image, (self.resolution, self.resolution))
         image = (image.astype(np.float32) / 127.5) - 1.0
 
         anno = self.annos[index]
-        local_files = []
-        for local_type in self.local_type_list:
-            local_files.append(self.local_paths[local_type][index])
         global_files = []
         for global_type in self.global_type_list:
             global_files.append(self.global_paths[global_type][index])
 
         local_conditions = []
-        for local_file in local_files:
-            condition = cv2.imread(local_file)
-            try:    
-                condition = cv2.cvtColor(condition, cv2.COLOR_BGR2RGB)
-            except:
-                print(local_file)
-            condition = cv2.resize(condition, (self.resolution, self.resolution))
-            condition = condition.astype(np.float32) / 255.0
-            local_conditions.append(condition)
+        local_image = cv2.imread(self.image_paths[index])
+        local_image = cv2.cvtColor(local_image, cv2.COLOR_BGR2RGB)
+        local_image = cv2.resize(local_image, (self.resolution, self.resolution))
+        local_image = local_image.astype(np.float32) / 255.0
+
+        local_conditions.append(local_image)
+
         global_conditions = []
         for global_file in global_files:
             condition = np.load(global_file)
             global_conditions.append(condition)
 
         if random.random() < self.drop_txt_prob:
-            anno = ''
-        local_conditions = keep_and_drop(local_conditions, self.keep_all_cond_prob, self.drop_all_cond_prob, self.drop_each_cond_prob)
-        global_conditions = keep_and_drop(global_conditions, self.keep_all_cond_prob, self.drop_all_cond_prob, self.drop_each_cond_prob)
+            anno = ""
+        # use only one condition (original image) for local_conditions
+        global_conditions = keep_and_drop(
+            global_conditions,
+            self.keep_all_cond_prob,
+            self.drop_all_cond_prob,
+            self.drop_each_cond_prob,
+        )
         if len(local_conditions) != 0:
             local_conditions = np.concatenate(local_conditions, axis=2)
         if len(global_conditions) != 0:
             global_conditions = np.concatenate(global_conditions)
 
-        return dict(jpg=image, txt=anno, local_conditions=local_conditions, global_conditions=global_conditions)
-        
+        return dict(
+            jpg=image,
+            txt=anno,
+            local_conditions=local_conditions,
+            global_conditions=global_conditions,
+        )
+
     def __len__(self):
         return len(self.annos)
-        
--- src/train/train.py
+++ src/train/train.py
@@ -1,36 +1,32 @@
 import sys
-if './' not in sys.path:
-	sys.path.append('./')
-	
+
+if "./" not in sys.path:
+    sys.path.append("./")
+
 from omegaconf import OmegaConf
 import argparse
-import os
-import torch
+
 from torch.utils.data import DataLoader
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
+import torch.multiprocessing
 
 from ldm.util import instantiate_from_config
 from models.util import load_state_dict
 from models.logger import ImageLogger
-import logging
 
-# Set up logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-parser = argparse.ArgumentParser(description='Uni-ControlNet Training')
-parser.add_argument('--config-path', type=str, default='./configs/global_v15.yaml')
-parser.add_argument('--learning-rate', type=float, default=1e-5)
-parser.add_argument('---batch-size', type=int, default=4)
-parser.add_argument('---training-steps', type=int, default=50000)
-parser.add_argument('---resume-path', type=str, default='./ckpt/init_global.ckpt')
-parser.add_argument('---logdir', type=str, default='./log_local/')
-parser.add_argument('---log-freq', type=int, default=10)
-parser.add_argument('---sd-locked', type=bool, default=True)
-parser.add_argument('---num-workers', type=int, default=4)
-parser.add_argument('---gpus', type=int, default=6)
-parser.add_argument('--ckpt', type=str, default='./checkpoints/')
+torch.multiprocessing.set_sharing_strategy("file_system")
+parser = argparse.ArgumentParser(description="Uni-ControlNet Training")
+parser.add_argument("--config-path", type=str, default="./configs/local_v15.yaml")
+parser.add_argument("--learning-rate", type=float, default=1e-5)
+parser.add_argument("---batch-size", type=int, default=1)
+parser.add_argument("---max-epochs", type=int, default=2)
+parser.add_argument("---resume-path", type=str, default="./ckpt/init_local.ckpt")
+parser.add_argument("---logdir", type=str, default="./log_local/")
+parser.add_argument("---log-freq", type=int, default=500)
+parser.add_argument("---sd-locked", type=bool, default=True)
+parser.add_argument("---num-workers", type=int, default=4)
+parser.add_argument("---gpus", type=int, default=-1)
 args = parser.parse_args()
 
 
@@ -39,7 +35,7 @@ def main():
     config_path = args.config_path
     learning_rate = args.learning_rate
     batch_size = args.batch_size
-    training_steps = args.training_steps
+    max_epochs = args.max_epochs
     resume_path = args.resume_path
     default_logdir = args.logdir
     logger_freq = args.log_freq
@@ -48,43 +44,39 @@ def main():
     gpus = args.gpus
 
     config = OmegaConf.load(config_path)
-    model = instantiate_from_config(config['model'])
+    model = instantiate_from_config(config["model"])
 
-    model.load_state_dict(load_state_dict(resume_path, location='cpu'))
+    model.load_state_dict(load_state_dict(resume_path, location="cpu"))
     model.learning_rate = learning_rate
     model.sd_locked = sd_locked
 
-    dataset = instantiate_from_config(config['data'])
-    dataloader = DataLoader(dataset, num_workers=num_workers, batch_size=batch_size, pin_memory=True, shuffle=True)
-    # cv2.setLogLevel(cv2.LOG_LEVEL_VERBOSE)
-    checkpoint_dir = args.ckpt
-    
-    # Ensure the checkpoint directory exists
-    if not os.path.exists(checkpoint_dir):
-        os.makedirs(checkpoint_dir)
-        print(f"Created checkpoint directory: {checkpoint_dir}")
+    dataset = instantiate_from_config(config["data"])
+    dataloader = DataLoader(
+        dataset,
+        num_workers=num_workers,
+        batch_size=batch_size,
+        pin_memory=True,
+        shuffle=True,
+        persistent_workers=True,
+    )
 
     logger = ImageLogger(batch_frequency=logger_freq)
     checkpoint_callback = ModelCheckpoint(
-        monitor='train/loss_epoch',
-        save_top_k=1,
-        mode='min',
-        dirpath=checkpoint_dir,
-        filename='best-checkpoint',
+        every_n_train_steps=logger_freq,
     )
-        
+
     trainer = pl.Trainer(
         gpus=gpus,
-        callbacks=[logger, checkpoint_callback], 
+        callbacks=[logger, checkpoint_callback],
         default_root_dir=default_logdir,
-        max_steps=training_steps,
+        max_epochs=max_epochs,
+        enable_progress_bar=False,
     )
-    print(f"Checkpoints will be saved to: {checkpoint_callback.dirpath}")
-
-    trainer.fit(model,
-        dataloader, 
+    trainer.fit(
+        model,
+        dataloader,
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
