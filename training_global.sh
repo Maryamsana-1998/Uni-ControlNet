81bf31235ce661fde973782ab1cde7becab431fa
Maryamsana-1998 <msana.bee17seecs@seecs.edu.pk>
2024-07-15 17:19:47 +0900

    updated paths and args

--- training_global.sh
+++ training_global.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 #SBATCH --time=4-0  # Set the time limit to 3 hours
-#SBATCH --gres=gpu:6
+#SBATCH --gres=gpu:8
 #SBATCH --cpus-per-gpu=8
 #SBATCH --mem-per-gpu=29G
 #SBATCH -p batch_grad
-#SBATCH -w ariel-v9
+#SBATCH -w ariel-v8
+#SBATCH -o slurm_logs/slurm-%A_%x.out
 
-#Your command to run the Python script
-#pip install torch torchvision compressai
-#source activate compressails
-python src/train/train.py --config-path ./configs/global_v15.yaml ---resume-path ./ckpt/init_global.ckpt --ckpt ./checkpoints/global_all ---training-steps 200000 ---gpus 6 ---logdir ./global_logs/
+python src/train/train.py --config-path ./configs/laicon/global_v15.yaml  ---resume-path ckpt/laicon/init_global.ckpt ---max-epochs 200000 ---gpus 8 ---logdir global_logs/
