261b3be3a607582bce8a7dad7749b0e1d100fa14
Maryamsana-1998 <msana.bee17seecs@seecs.edu.pk>
2024-07-19 19:17:52 +0900

    updated

--- training_global.sh
+++ training_global.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --time=4-0  # Set the time limit to 3 hours
-#SBATCH --gres=gpu:8
+#SBATCH --gres=gpu:6
 #SBATCH --cpus-per-gpu=8
 #SBATCH --mem-per-gpu=29G
 #SBATCH -p batch_grad
-#SBATCH -w ariel-v8
+#SBATCH -w ariel-k2
 #SBATCH -o slurm_logs/slurm-laion-%A_%x.out
 
-python src/train/train.py --config-path ./configs/laicon/global_v15.yaml  ---resume-path ckpt/laicon/init_global.ckpt ---max-epochs 3  ---gpus 8 ---logdir global_logs/
+python src/train/train2.py --config-path ./configs/laicon/global_v15.yaml  ---resume-path ckpt/laicon/init_global.ckpt ---max-epochs 3  ---gpus 6 ---logdir global_logs/
