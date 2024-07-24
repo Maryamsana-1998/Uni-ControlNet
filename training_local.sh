b4c54c08ced89801c165b81db1ba5e7bdbddce88
Maryamsana-1998 <msana.bee17seecs@seecs.edu.pk>
2024-07-24 18:14:31 +0900

    updated

--- training_local.sh
+++ training_local.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-#SBATCH --time=4-0  # Set the time limit to 3 hours
-#SBATCH --gres=gpu:6
+#SBATCH --time=6-0  # Set the time limit to 3 hours
+#SBATCH --gres=gpu:8
 #SBATCH --cpus-per-gpu=8
 #SBATCH --mem-per-gpu=29G
 #SBATCH -p batch_grad
-#SBATCH -w ariel-v13
+#SBATCH -w ariel-v4
+#SBATCH -o slurm_logs/slurm-vimeo-%A_%x.out
+#SBATCH -e slurm_logs/slurm-vimeo-%A_%x.err
+
 
 #Your command to run the Python script
 #pip install torch torchvision compressai
 #source activate compressails
-python src/train/train.py --config-path ./configs/vimeo_img2img/local_v15.yaml ---resume-path ckpt/vimeo_img2img/init_local.ckpt --ckpt ./checkpoints/vimeo_img2img/local/ ---gpus 6 ---logdir logs/vimeo_img2img/local/
+python src/train/train.py --config-path ./configs/vimeo_img2img/local_v15.yaml ---resume-path ckpt/vimeo_img2img/init_local.ckpt --ckpt ./checkpoints/vimeo_img2img/local/ ---gpus 8 ---logdir logs/vimeo_img2img/local/
 
