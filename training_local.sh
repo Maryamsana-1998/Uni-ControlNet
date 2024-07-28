282e925b8bbdc45cb88bcf7aa4bed86c0413fb00
Maryamsana-1998 <msana.bee17seecs@seecs.edu.pk>
2024-07-28 16:49:59 +0900

    training im2im done

--- training_local.sh
+++ training_local.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-gpu=8
 #SBATCH --mem-per-gpu=29G
 #SBATCH -p batch_grad
-#SBATCH -w ariel-v4
+#SBATCH -w ariel-k2
 #SBATCH -o slurm_logs/slurm-vimeo-%A_%x.out
 #SBATCH -e slurm_logs/slurm-vimeo-%A_%x.err
 
@@ -12,5 +12,5 @@
 #Your command to run the Python script
 #pip install torch torchvision compressai
 #source activate compressails
-python src/train/train.py --config-path ./configs/vimeo_img2img/local_v15.yaml ---resume-path ckpt/vimeo_img2img/init_local.ckpt --ckpt ./checkpoints/vimeo_img2img/local/ ---gpus 8 ---logdir logs/vimeo_img2img/local/
+python src/train/train.py --config-path ./configs/vimeo_img2img/local_v15.yaml ---resume-path ckpt/vimeo_img2img/init_local.ckpt ---gpus 8 ---logdir logs/vimeo_img2img/local/
 
