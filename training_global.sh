86aab79fe5064bb9c35a131993bae792ef9c20d6
Maryamsana-1998 <msana.bee17seecs@seecs.edu.pk>
2024-07-31 17:48:34 +0900

    training canny+Seg

--- training_global.sh
+++ training_global.sh
@@ -5,10 +5,10 @@
 #SBATCH --mem-per-gpu=29G
 #SBATCH -p batch_grad
 #SBATCH -w ariel-v8
-#SBATCH -o slurm_logs/slurm-vimeo-%A_%x.out
-#SBATCH -e slurm_logs/slurm-vimeo-%A_%x.err
+#SBATCH -o slurm_logs/slurm-vimeo_cs-%A_%x.out
+#SBATCH -e slurm_logs/slurm-vimeo_cs-%A_%x.err
 
 #Your command to run the Python script
 #pip install torch torchvision compressai
 #source activate compressails
-python src/train/train_global.py --config-path ./configs/vimeo_img2img/global_v15.yaml ---resume-path ./ckpt/vimeo_img2img/init_global.ckpt ---gpus 6 ---logdir logs/vimeo_img2img/global
+python src/train/train_global.py --config-path ./configs/vimeo/global_v15.yaml ---resume-path ./ckpt/vimeo/init_global.ckpt ---gpus 6 ---logdir logs/vimeo/global
--- training_local.sh
+++ training_local.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --time=6-0  # Set the time limit to 3 hours
-#SBATCH --gres=gpu:8
+#SBATCH --gres=gpu:4
 #SBATCH --cpus-per-gpu=8
 #SBATCH --mem-per-gpu=29G
 #SBATCH -p batch_grad
 #SBATCH -w ariel-k2
-#SBATCH -o slurm_logs/slurm-vimeo-%A_%x.out
-#SBATCH -e slurm_logs/slurm-vimeo-%A_%x.err
+#SBATCH -o slurm_logs/slurm-vimeo_cs-%A_%x.out
+#SBATCH -e slurm_logs/slurm-vimeo_cs-%A_%x.err
 
 
 #Your command to run the Python script
 #pip install torch torchvision compressai
 #source activate compressails
-python src/train/train.py --config-path ./configs/vimeo_img2img/local_v15.yaml ---resume-path ckpt/vimeo_img2img/init_local.ckpt ---gpus 8 ---logdir logs/vimeo_img2img/local/
+python src/train/train.py --config-path ./configs/vimeo/local_v15.yaml ---resume-path ckpt/vimeo/init_local.ckpt ---gpus 4 ---logdir logs/vimeo/local/
 
