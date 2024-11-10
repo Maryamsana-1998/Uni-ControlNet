4dd40ca540747728be14b184acac8dfc2525e26f
Maryamsana-1998 <msana.bee17seecs@seecs.edu.pk>
2024-11-10 18:48:51 +0900

    added lpips in ddpm and two configs with perceptual weights

--- training_local_percep.sh
+++ training_local_percep.sh
@@ -4,16 +4,16 @@
 #SBATCH --cpus-per-gpu=8
 #SBATCH --mem-per-gpu=29G
 #SBATCH -p batch_grad
-#SBATCH -w ariel-v11
+#SBATCH -w ariel-v13
 #SBATCH -o slurm_logs/slurm-%A_%x.out
 #SBATCH -e slurm_logs/slurm-%A_%x.err
 
 python src/train/train.py \
-    --config-path configs/vimeo_vgg_percep/local_v15.yaml \
+    --config-path configs/vimeo_vgg_percep/local_v15_percep.yaml \
     ---resume-path ckpt/vimeo/init_local.ckpt \
     ---max-epochs 1 \
     ---batch-size 3 \
     ---gpus 8 \
     ---logdir logs/vgg/ \
-    --checkpoint-dirpath checkpoints/vimeo_vgg_0/
+    --checkpoint-dirpath checkpoints/vimeo_vgg_0.05/
 
